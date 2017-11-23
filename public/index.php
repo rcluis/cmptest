90cfa53a017a18bb612956aac853cdd16d7f104e
Unknown <raulcatalanluis@gmail.com>
2017-11-23 21:42:28 +0100

    refactor: delete unused code

--- public/index.php
+++ public/index.php
@@ -4,11 +4,4 @@ use Cmptest\App;
 
 $app = (new App())->get();
 
-// $app->get('/', function (Request $request, Response $response) {
-//     // $name = $request->getAttribute('name');
-//     // $response->getBody()->write("Hello, $name");
-//
-//     $response = $this->view->render($response, "profile.phtml");
-//     return $response;
-// });
 $app->run();
